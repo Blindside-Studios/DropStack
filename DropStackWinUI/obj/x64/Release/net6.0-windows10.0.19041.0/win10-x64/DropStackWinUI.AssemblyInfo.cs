5a3ef3114755d0ff2cfb1d4cd2b085c3cb7dbfe9
Blindside Studios <64647061+Blindside-Studios@users.noreply.github.com>
2024-03-29 21:32:02 +0100

    Hid Camera feature for bugfix release
    Also disabled webcam capability.

--- DropStackWinUI/obj/x64/Release/net6.0-windows10.0.19041.0/win10-x64/DropStackWinUI.AssemblyInfo.cs
+++ DropStackWinUI/obj/x64/Release/net6.0-windows10.0.19041.0/win10-x64/DropStackWinUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DropStackWinUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57ebbb8ea1f11964094d7bd361c42ec7ec584953")]
 [assembly: System.Reflection.AssemblyProductAttribute("DropStackWinUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DropStackWinUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DropStackWinUI/obj/x86/Debug/net6.0-windows10.0.19041.0/win10-x86/DropStackWinUI.AssemblyInfo.cs
+++ DropStackWinUI/obj/x86/Debug/net6.0-windows10.0.19041.0/win10-x86/DropStackWinUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DropStackWinUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acf0c32370e3d41cdb021545798a20cbdb38a481")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57ebbb8ea1f11964094d7bd361c42ec7ec584953")]
 [assembly: System.Reflection.AssemblyProductAttribute("DropStackWinUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DropStackWinUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
